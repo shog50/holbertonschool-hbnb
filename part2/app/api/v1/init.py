090b0d3c851202bdea91c77b2b32d95649447826
shog50 <10709@holbertonstudents.com>
2025-06-17 17:05:44 +0300

    Update init.py

--- part2/app/api/v1/init.py
+++ part2/app/api/v1/init.py
@@ -1,1 +1,11 @@
-# Empty file to make directory a Python package
+from flask import Blueprint
+from flask_restx import Api
+
+api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+api = Api(api_v1, version='1.0', title='My API', description='A simple API')
+
+from .places import api as places_api
+from .reviews import api as reviews_api
+
+api.add_namespace(places_api)
+api.add_namespace(reviews_api)
