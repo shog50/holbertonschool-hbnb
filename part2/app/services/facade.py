53498984e52051c417e350fb9efaa8c66c70b90a
shog50 <10709@holbertonstudents.com>
2025-06-17 16:52:56 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -16,3 +16,27 @@ class HBnBFacade:
     def get_place(self, place_id):
         """Placeholder for getting a place"""
         pass
+
+    def create_review(self, review_data):
+        """Placeholder for creating a review"""
+        pass
+
+    def get_review(self, review_id):
+        """Placeholder for getting a review"""
+        pass
+
+    def get_all_reviews(self):
+        """Placeholder for getting all reviews"""
+        pass
+
+    def get_reviews_by_place(self, place_id):
+        """Placeholder for getting reviews by place"""
+        pass
+
+    def update_review(self, review_id, review_data):
+        """Placeholder for updating a review"""
+        pass
+
+    def delete_review(self, review_id):
+        """Placeholder for deleting a review"""
+        pass
